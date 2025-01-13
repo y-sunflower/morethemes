fe922fdced0385ae2889c0d5e746a2261dbc60c4
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-01-13 15:46:59 +0100

    V0

--- img/make.py
+++ img/make.py
@@ -1,20 +1,11 @@
-# `morethemes`: more themes for matplotlib
-
-<br />
-
-### Quick start
-
-```python
 import matplotlib.pyplot as plt
 import morethemes as mt
+
+plt.rcParams["savefqig.dpi"] = 200
 
 mt.set_theme("darker")
 
 fig, ax = plt.subplots()
 ax.barh(["Yes", "No"], [10, 20])
+plt.savefig("img/quickstart.png", bbox_inches="tight")
 plt.show()
-```
-
-![](img/quickstart.png)
-
-<br />
